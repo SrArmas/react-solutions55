c3f31b8fa1d200cf4760ce28f83b515575e20b27
karlaevelize <k.evelize@gmail.com>
2021-12-03 19:06:03 +0100

    Required features complete

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { Routes, Route } from "react-router-dom"
-import Characters from './pages/Characters';
+import Homepage from "./pages/Homepage"
 import CharDetails from './pages/CharDetails';
 
 function App() {
@@ -10,7 +10,7 @@ function App() {
       <Routes>
         {/* dynamic route, to acces the characterId we will need useParams()  */}
         <Route path="/character/:characterId" element={<CharDetails/>}/>
-        <Route exact path="/" element={<Characters/>}/>
+        <Route exact path="/" element={<Homepage/>}/>
       </Routes>
     </div>
   );
